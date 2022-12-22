2082d00b45073af9fe0519ef0f57e31fc3516ceb
Gustavo yuki Miyakawa <gmiyakaw@c1r8p8.42quebec.com>
2022-12-22 15:44:04 -0500

    DEC 22

--- pipex.h
+++ pipex.h
@@ -6,9 +6,34 @@
 /*   By: gmiyakaw <gmiyakaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:17:36 by gmiyakaw          #+#    #+#             */
-/*   Updated: 2022/12/22 15:20:16 by gmiyakaw         ###   ########.fr       */
+/*   Updated: 2022/12/22 15:38:22 by gmiyakaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
+/* Here we go, brand new project. 
+
+First steps:
+Read assignment.
+Freak out because it doesn't make sense
+read about it
+freak out some more.
+keep reading and watching tutorials
+freak out less
+code it all in
+debug
+change your mind, restructure, restart
+debug some more
+win
+
+freak out because code is working
+remember impostor syndrome is a thing
+stop freaking out
+
+push project and subcribe for defense
+
+update github
+
+start a new project.
+*/
--- tests.c
+++ tests.c
@@ -6,7 +6,15 @@
 /*   By: gmiyakaw <gmiyakaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:17:16 by gmiyakaw          #+#    #+#             */
-/*   Updated: 2022/12/22 15:17:17 by gmiyakaw         ###   ########.fr       */
+/*   Updated: 2022/12/22 15:41:21 by gmiyakaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+allowed functions:
+open, close, read, write,
+malloc, free, perror,
+strerror, access, dup, dup2,
+execve, exit, fork, pipe,
+unlink, wait, waitpid
+*/
