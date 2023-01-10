b7c24a39f694a9ac041412e578f335e0ec570e83
gymiyakawa laptop <gymiyakawa@Hungry-Baku.localdomain>
2023-01-10 10:17:43 -0500

    test

--- pipex.c
+++ pipex.c
@@ -6,9 +6,19 @@
 /*   By: gmiyakaw <gmiyakaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:17:01 by gmiyakaw          #+#    #+#             */
-/*   Updated: 2022/12/22 15:17:04 by gmiyakaw         ###   ########.fr       */
+/*   Updated: 2022/12/26 09:47:44 by gmiyakaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+much of this project will be about parsing this info
 
+organize argvs into categories, but waht comes after?
+gotta recoginize "and then execute the commands in it.
+
+we'll be reading from files as well. Gotta touch up on that.
+
+
+
+*/
 
--- pipex.h
+++ pipex.h
@@ -6,9 +6,11 @@
 /*   By: gmiyakaw <gmiyakaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:17:36 by gmiyakaw          #+#    #+#             */
-/*   Updated: 2022/12/22 15:20:16 by gmiyakaw         ###   ########.fr       */
+/*   Updated: 2023/01/04 11:29:20 by gmiyakaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdio.h>
+#include <unistd.h>
 
